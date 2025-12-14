5d92a46f0bbd3d89dd89e5876fc4771a5743f3a6
luke-d-moore <luke.d.moore1@gmail.com>
2025-12-14 14:21:17 +0000

    add health check to make sure the pricingsystem is up and running before the other apps start
    add health check to make sure the pricingsystem is up and running before the other apps start

--- Aspire-AutoTradeSystem/AppHost.cs
+++ Aspire-AutoTradeSystem/AppHost.cs
@@ -1,11 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var pricingSystem = builder.AddProject<Projects.PricingSystem>("PricingSystem");
+var pricingSystem = builder.AddProject<Projects.PricingSystem>("PricingSystem")
+                             .WithHttpHealthCheck("/api/price/GetAllPrices");
 
 builder.AddProject<Projects.AutoTradeSystem>("AutoTradeSystem")
-                             .WithReference(pricingSystem);
+                             .WithReference(pricingSystem)
+                             .WaitFor(pricingSystem);
 
 builder.AddProject<Projects.TradeActionSystem>("TradeActionSystem")
-                               .WithReference(pricingSystem);
+                               .WithReference(pricingSystem)
+                               .WaitFor(pricingSystem);
 
 builder.Build().Run();
